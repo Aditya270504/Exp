d9123b8247db94d71cd11e3012a01ce645d3789a
Aditya270504 <aditya.jha027@gmail.com>
2025-04-23 07:14:21 +0530

    Second  Release

---
+++ multiply.py
@@ -0,0 +1,15 @@
+def multiply(a, b):
+    """
+    Multiplies two numbers.
+
+    Parameters:
+    a (int or float): The first number.
+    b (int or float): The second number.
+
+    Returns:
+    int or float: The product of a and b.
+    """
+    return a * b
+
+multiply(2, 3)
+multiply(2.5, 3.5)
