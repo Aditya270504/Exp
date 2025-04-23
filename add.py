2b83d5bc16f53c9f40245e9ab18646484da9fe99
Aditya270504 <aditya.jha027@gmail.com>
2025-04-23 07:01:49 +0530

    First Release

---
+++ add.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    """
+    Adds two numbers together.
+
+    Parameters:
+    a (int or float): The first number.
+    b (int or float): The second number.
+
+    Returns:
+    int or float: The sum of a and b.
+    """
+    return a + b
+
+add(1, 2)
+add(1.5, 2.5)
---
+++ subtratct.py
@@ -0,0 +1,15 @@
+def subtract(a, b):
+    """
+    Subtracts two numbers.
+
+    Parameters:
+    a (int, float): The first number.
+    b (int, float): The second number.
+
+    Returns:
+    int, float: The result of subtracting b from a.
+    """
+    return a - b
+
+subtract(5, 3)
+subtract(5.5, 3.5)
